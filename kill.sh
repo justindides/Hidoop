1c9d44a28875f5554bf8714de6ef46202eefda2c
cgarosail <camille.garosail@gmail.com>
2020-02-21 11:07:09 +0100

    Utilisation d'une nouvelle machine pour le namenode

--- kill.sh
+++ kill.sh
@@ -6,8 +6,16 @@ DAEMON1='malicia'${SUFFIX}
 DAEMON2='yoda'${SUFFIX}
 DAEMON3='palpatine'${SUFFIX}
 DAEMON4='torvalds'${SUFFIX}
+NAMENODE='liskov'${SUFFIX}
 
-ssh cgarosai@${DAEMON1} "pkill java"&
-ssh cgarosai@${DAEMON2} "pkill java"&
-ssh cgarosai@${DAEMON3} "pkill java"&
-ssh cgarosai@${DAEMON4} "pkill java"&
+ssh cgarosai@${DAEMON1} "pkill java"
+echo  "Daemon1 killed"
+ssh cgarosai@${DAEMON2} "pkill java"
+echo  "Daemon2 killed"
+ssh cgarosai@${DAEMON3} "pkill java"
+echo  "Daemon3 killed"
+ssh cgarosai@${DAEMON4} "pkill java"
+echo  "Daemon4 killed"
+ssh cgarosai@${NAMENODE} "pkill java"
+echo  "Namenode killed"
+
